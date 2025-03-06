372205f72396860a1a2fb3d1f2864e5035efec74
Aleksey <pnev.leha@mail.ru>
2025-03-06 22:34:25 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import styles from './App.module.scss'
+
+function App() {
+
+  return (
+    <main className={styles.main}>
+        <div className={styles.sidebar}>
+            side
+        </div>
+        <div className={styles.content}>
+            main
+        </div>
+    </main>
+  )
+}
+
+export default App
